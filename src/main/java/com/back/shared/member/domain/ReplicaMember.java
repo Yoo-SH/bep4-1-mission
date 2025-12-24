471b821874ded331e1fc2e7ec5c4fc139d9c4b74
Yoo-SH <ads137485@jbnu.ac.kr>
2025-12-24 17:33:53 +0900

    0018

--- src/main/java/com/back/shared/member/domain/ReplicaMember.java
+++ src/main/java/com/back/shared/member/domain/ReplicaMember.java
@@ -4,15 +4,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-import static lombok.AccessLevel.PROTECTED;
-
 @MappedSuperclass
 @Getter
-@Setter(value = PROTECTED)
 @NoArgsConstructor
 public abstract class ReplicaMember extends BaseMember {
     @Id
@@ -20,7 +16,10 @@ public abstract class ReplicaMember extends BaseMember {
     private LocalDateTime createDate;
     private LocalDateTime modifyDate;
 
-    public ReplicaMember(String username, String password, String nickname) {
+    public ReplicaMember(int id, LocalDateTime createDate, LocalDateTime modifyDate, String username, String password, String nickname) {
         super(username, password, nickname);
+        this.id = id;
+        this.createDate = createDate;
+        this.modifyDate = modifyDate;
     }
 }
