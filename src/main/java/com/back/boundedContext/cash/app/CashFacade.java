2254f6eb48e129a86446c0d18858f2e5b171fc6c
Yoo-SH <ads137485@jbnu.ac.kr>
2025-12-26 14:22:54 +0900

    0026

--- src/main/java/com/back/boundedContext/cash/app/CashFacade.java
+++ src/main/java/com/back/boundedContext/cash/app/CashFacade.java
@@ -1,3 +1,4 @@
+
 package com.back.boundedContext.cash.app;
 
 import com.back.boundedContext.cash.domain.CashMember;
@@ -8,6 +9,8 @@ import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -35,5 +38,15 @@ public class CashFacade {
         Wallet wallet = new Wallet(holder);
 
         return walletRepository.save(wallet);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<CashMember> findMemberByUsername(String username) {
+        return cashMemberRepository.findByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Wallet> findWalletByHolder(CashMember holder) {
+        return walletRepository.findByHolder(holder);
     }
 }
