a4c4cbce80f868d9a39a40813cb95e79cf524233
Yoo-SH <ads137485@jbnu.ac.kr>
2025-12-30 19:14:35 +0900

    0045

--- src/main/java/com/back/boundedContext/payout/out/PayoutRepository.java
+++
@@ -1,7 +0,0 @@
-package com.back.boundedContext.payout.out;
-
-import com.back.boundedContext.payout.domain.Payout;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PayoutRepository extends JpaRepository<Payout, Integer> {
-}
